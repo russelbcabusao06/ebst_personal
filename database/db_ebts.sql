26b75d234d297f8d66ddb891d50fa38426a21c5c
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-02 11:59:50 +0800

    ebts

--- database/db_ebts.sql
+++ database/db_ebts.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_ebts`
 --
-CREATE DATABASE `db_ebst`;
-USE `db_ebst`;
+CREATE DATABASE `db_ebts`;
+USE `db_ebts`;
 -- --------------------------------------------------------
 
 --
