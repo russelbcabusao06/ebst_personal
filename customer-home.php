9ea3c69949e4f298bb996a5e06dfa62f53086c90
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-11 14:01:44 +0800

    c

--- customer-home.php
+++ customer-home.php
@@ -73,7 +73,7 @@ require_once("config.php");
       display: grid;
       grid-gap: 9.1em;
       grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-      background-image: url('crop image-customer/1.png')
+      image: ('crop image-customer/1.png')
     }
     .quote {
       padding: 1em;
