454bda29b6f6ecb8c4f8d6846308abf989c5df65
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-11 13:00:55 +0800

    c

--- customer-home.php
+++ customer-home.php
@@ -319,7 +319,6 @@ require_once("config.php");
   <img src="crop image-customer/2.png">
   <img src="crop image-customer/3.png">
   <img src="crop image-customer/4.png">
-  <img src="crop image-customer/4.png">
   <img src="crop image-customer/5.png">
   <img src="crop image-customer/6.png">
   <img src="crop image-customer/7.png">
