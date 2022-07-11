7d44c68b8a52de3f252e22f95e413268ea88149d
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-11 13:12:32 +0800

    c

--- customer-home.php
+++ customer-home.php
@@ -331,7 +331,7 @@ require_once("config.php");
     $stat = $dbh->prepare("SELECT * from tbl_picture");
     $stat->execute();
     while ($row = $stat->fetch()) {
-      echo "<div class='quote'><embed src='data:image/jpg" . ";base64," . base64_encode($row['data']) . "' width='350' height='230' />";
+      echo "<div class='quote'><embed src='crop image-customer/1.png" . ";base64," . base64_encode($row['data']) . "' width='350' height='230' />";
       echo "<h3 style='width:100%;font-size:1.8em;'><b><center>NAME OF CONDO</center></b></h3>";
       echo "<h3 style='font-size:1.17em;overflow:auto;font-family:Moonglade;'><br><img src='image/location-removebg-preview.png' style='width:6%;'>" . $row['location'] . "</h3>";
       echo "<div style='margin-left:60%;margin-top:-10%;'>";
