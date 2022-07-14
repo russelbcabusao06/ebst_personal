82e681263157e10f7d324de8b99e6f99236a2983
russelbcabusao06 <russel.cabusao.b@bulsu.edu.ph>
2022-07-14 12:01:36 +0800

    r

--- customer-home.php
+++ customer-home.php
@@ -318,12 +318,6 @@ require_once("config.php");
   <div class="container">
     <?php
     $sql = "SELECT * from tbl_picture;";
-    $result = mysqli_query($sql);
-    if($result->num_rows>0){
-      while($row = $result->fetch_assoc($result)){?>
-      <?php echo $row['name']?>
-      <?php
-    }}
     $dbh = new PDO("mysql:host=localhost;dbname=db_ebts", "root", "");
     $stat = $dbh->prepare("SELECT * from tbl_picture");
     $stat->execute();
